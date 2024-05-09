feaaab7e6b9314b321355bd226897fc3d6b20cff
Oussama Mahmoud <oussama.mahmoud@gmail.com>
2024-05-10 00:36:37 +0100

    0.2.3.240510
    - Add pages design
    - Add dependencies for JQuery and Bootstrap
    - Start the Authentication Process
    - Bug Fix

--- src/main/java/com/kingoma/controllers/HomeController.java
+++ src/main/java/com/kingoma/controllers/HomeController.java
@@ -1,6 +1,6 @@
 package com.kingoma.controllers;
 
-import org.springframework.security.access.prepost.PreAuthorize;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class HomeController {
 
-    @PreAuthorize("hasRole('ROLE_PATIENT')")
     @GetMapping("/")
     public String showIndexPage(Model model) {
         return "Index";
     }
+    /*public String showIndexPage(Model model) {
+        return "authentication-login";
+    }*/
 }
